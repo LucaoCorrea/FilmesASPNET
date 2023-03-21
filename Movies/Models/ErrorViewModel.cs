659a412907224a677ab330b37337b8137055ae87
will9191 <willian.fernando040@gmail.com>
2023-03-21 12:27:23 -0300

    Fazendo btn do card

--- Movies/Models/ErrorViewModel.cs
+++ Movies/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Movies.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
